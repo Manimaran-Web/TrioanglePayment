fa21b93b63809d8531e2fb10df3477c072597c33
pvignesh <pvignesh90@gmail.com>
2021-04-03 16:14:40 +0530

    First commit**

---
+++ README.md
@@ -0,0 +1,1 @@
+#This payment package common for all projects
---
+++ src/MakePayment.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ 
+ * @author      Manimaran
+ 
+ */
+
+namespace Msquare\TrioanglePayment;
+
+class MakePayment 
+{
+
+	 public function index(String $sName)
+    {
+        return 'Hi ' . $sName . '! How are you doing today?';
+    }
+
+}
